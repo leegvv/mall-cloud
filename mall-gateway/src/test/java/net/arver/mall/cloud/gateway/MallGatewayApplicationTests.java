08404ec0597766f0876d89cce64b28587f5816a5
leegvv <leegvv@163.com>
2020-08-03 22:07:52 +0800

    Initial commit

---
+++ mall-gateway/src/test/java/net/arver/mall/cloud/gateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.arver.mall.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-registry/src/test/java/net/arver/mall/cloud/registry/MallRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.arver.mall.cloud.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
